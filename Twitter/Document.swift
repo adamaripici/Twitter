b35363ccf81fab23a71bfcf9862e100d85100351
adamaripici <55006272+adamaripici@users.noreply.github.com>
2021-10-03 22:34:12 -0500

    Initial Commit

---
+++ Twitter/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  Twitter
+//
+//  Created by Ada Pici on 10/3/21.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
